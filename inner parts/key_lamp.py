5fddccf8e80bd89c19df13e4e63f5cbcf5c06d3a
Ali Khoshroo <mradriawn@gmail.com>
2024-09-06 13:05:25 +0330

    Create Key board & Lamp board

---
+++ inner parts/key_lamp.py
@@ -0,0 +1,17 @@
+import string
+letters = string.ascii_uppercase
+
+class KeyBoard_Lamp():
+
+    def __init__(self) -> None:
+        pass
+
+    def forward(letter):
+        position = letters.find(letter)
+        return position
+    
+    def backward(position):
+        position = int(position)
+        letter = letters[position]
+        return letter
+    
